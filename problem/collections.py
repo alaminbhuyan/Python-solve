45f052fd2f98cf635420c934a2996ac687394f75
alamin <alaminbhuyan321@gmail.com>
2020-04-01 10:50:40 +0600

    add file problem file

--- problem/collections.py
+++ problem/collections.py
@@ -1,9 +1,5 @@
-from collections import Counter
-lis = [1,1,2,3,4,5,3,4,2,1,2,3]
-print(Counter(lis))
-print(Counter(lis).items())
-print(Counter(lis).keys())
-print(Counter(lis).values())
+
+
 # num_of_shoes = int(input('num of shoes: '))
 # all_shoes_size = list(map(int,input().split()))
 # l = Counter(all_shoes_size)
@@ -11,3 +7,6 @@ print(Counter(lis).values())
 # num_of_customers = int(input('num of customers: '))
 #for i in range(1,num_of_customers+1):
 #print('hello world')
+from _collections import Counter
+lis = [1,2,1,3,4,2,2,3,3]
+print(list(Counter(lis)))
