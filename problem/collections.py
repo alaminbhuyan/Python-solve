09e172a765eb15db8297d45179c7ed4aeb9ac6e0
alamin <alaminbhuyan321@gmail.com>
2020-04-01 17:28:36 +0600

    work on problem file

--- problem/collections.py
+++ problem/collections.py
@@ -1,12 +1,3 @@
-
-
-# num_of_shoes = int(input('num of shoes: '))
-# all_shoes_size = list(map(int,input().split()))
-# l = Counter(all_shoes_size)
-# print(l)
-# num_of_customers = int(input('num of customers: '))
-#for i in range(1,num_of_customers+1):
-#print('hello world')
-from _collections import Counter
-lis = [1,2,1,3,4,2,2,3,3]
-print(list(Counter(lis)))
+from collections import Counter
+lis   = [1,1,1,2,2,2,3,3,3]
+print(Counter(lis))
--- program/pratice.py
+++ program/pratice.py
@@ -1,14 +1,4 @@
-# from calendar import weekday
-# mm , dd , yy = map(int,input().split())
-# day_num = weekday(yy,mm,dd)
-# print(day_num)
-# days =["Monday", "Tuesday", "Wednesday", "Thursday","Friday", "Saturday", "Sunday"]
-# print(days[day_num].upper())
 
-from pyqrcode import *
-
-s = "www.facebook.com"
-
-url = create(s)
-
-url.svg("my.png",scale=8)
+d = {6:4}
+d[6] -= 1
+print(d[6])
