2f884077e5fbb90c95d21ecaf568a58f0f24c57b
al-aminbhuyan <alaminbhuyan321@gmail.com>
2020-04-02 06:09:54 +0600

    add some file problem and program folder

--- problem/collections.py
+++ problem/collections.py
@@ -1,3 +1,15 @@
 from collections import Counter
-lis   = [1,1,1,2,2,2,3,3,3]
-print(Counter(lis))
+
+num_of_shoes = int(input())
+shoes_size = Counter(map(int, input().split()))
+num_of_customer = int(input())
+
+income = 0
+
+for i in range(num_of_customer):
+    size, price = map(int, input().split())
+    if shoes_size[size]:
+        income += price
+        shoes_size[size] -= 1
+
+print (income)
--- program/pratice.py
+++ program/pratice.py
@@ -1,4 +1,24 @@
 
-d = {6:4}
-d[6] -= 1
-print(d[6])
+# def fun1(n):
+#     def fun2(m):
+#         return m*n
+#     return fun2
+#
+# obj1 = fun1(5)
+# obj2 = fun1(5)
+#
+# print(obj1(2))
+# print(obj2(3))
+# print(obj1(obj2(4)))
+from primePy import primes
+
+print(primes.check(5))
+print(primes.check(10))
+print(primes.factor(15))
+print(primes.factors(15))
+print(primes.first(15))
+print(primes.upto(15))
+print(primes.between(50,100))
+print(primes.phi(15))
+print(primes.sqrt(5))
+print(primes.about())
