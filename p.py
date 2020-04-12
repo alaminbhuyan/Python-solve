89f976b118d100278c4531658b0ee52cc3707264
al-aminbhuyan <alaminbhuyan321@gmail.com>
2020-04-12 19:26:41 +0600

    modify some text

--- p.py
+++ p.py
@@ -16,10 +16,8 @@
 from collections import OrderedDict
 d = OrderedDict()
 for _ in range(int(input())):
-    item, space, quantity = input().rpartition(' ')
-    d[item] = d.get(item, 0) + int(quantity)
-for item, quantity in d.items():
-     print(item, quantity)
+    items, quantity = input().split()
+    d[items] = d.get(items,0)+int(quantity)
 
-
-
+for items,quantity in d.items():
+    print(items,quantity)
